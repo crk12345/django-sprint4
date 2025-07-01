e09f2724bbf5d633a1ba77bcabf5b990dc153c0b
igor <crk@mail.ru>
2025-07-01 22:07:19 +0300

    mode

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,104 +1,155 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-
-from blog.constants import SELFTITLELEN
+from django.urls import reverse
+from django.utils import timezone
 
 User = get_user_model()
 
 
-class CommonFieldsModel(models.Model):
-    """База для  модели"""
+class PostManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .select_related(
+                "category",
+                "author",
+                "location",
+            )
+            .filter(
+                is_published=True,
+                category__is_published=True,
+                pub_date__lte=timezone.now(),
+            )
+            .order_by("-pub_date")
+        )
 
+
+# Почему-то начал игнорировать (не сортирует)
+# ordering в метаклассе класса Post.
+# Пришлось тут добавить order_by.
+
+
+class BaseModel(models.Model):
     is_published = models.BooleanField(
+        verbose_name="Опубликовано",
         default=True,
-        verbose_name='Опубликовано',
-        help_text='Снимите галочку, чтобы скрыть публикацию.',
+        help_text="Снимите галочку, чтобы скрыть публикацию.",
     )
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name='Добавлено'
+        verbose_name="Добавлено", auto_now_add=True, auto_now=False
     )
 
     class Meta:
         abstract = True
 
 
-class BaseTitle(models.Model):
-    """База для заголовка"""
-
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-
-    class Meta:
-        abstract = True
-
-
-class Location(CommonFieldsModel):
-    """Местоположение"""
-
-    name = models.CharField(max_length=256, verbose_name="Название места")
-
-    class Meta:
-        verbose_name = "местоположение"
-        verbose_name_plural = "Местоположения"
-
-    def __str__(self):
-        return self.name[:SELFTITLELEN]
-
-
-class Category(CommonFieldsModel, BaseTitle):
-    """Категория"""
-
+class Category(BaseModel):
+    title = models.CharField(max_length=256, verbose_name="Заголовок")
     description = models.TextField(verbose_name="Описание")
     slug = models.SlugField(
-        unique=True,
         verbose_name="Идентификатор",
         help_text=(
-            "Идентификатор страницы для URL; "
-            "разрешены символы латиницы, цифры, дефис и подчёркивание."
+            "Идентификатор страницы для URL; разрешены символы "
+            "латиницы, цифры, дефис и подчёркивание."
         ),
+        unique=True,
     )
 
     class Meta:
         verbose_name = "категория"
         verbose_name_plural = "Категории"
 
-    def __str__(self):
-        return self.title[:SELFTITLELEN]
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self) -> str:
+        return reverse(
+            "blog:category_posts", kwargs={"category_slug": self.slug}
+        )
 
 
-class Post(CommonFieldsModel, BaseTitle):
-    """Публикация."""
+class Location(BaseModel):
+    name = models.CharField(max_length=256, verbose_name="Название места")
 
+    class Meta:
+        verbose_name = "местоположение"
+        verbose_name_plural = "Местоположения"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=256, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Текст")
     pub_date = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=False,
         verbose_name="Дата и время публикации",
-        help_text=("Если установить дату и время в будущем — "
-                   "можно делать отложенные публикации."),
+        help_text=(
+            "Если установить дату и время в будущем — можно делать "
+            "отложенные публикации."
+        ),
     )
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE,
-        related_name="posts",
         verbose_name="Автор публикации",
+        on_delete=models.CASCADE,
     )
     location = models.ForeignKey(
         Location,
+        verbose_name="Местоположение",
         on_delete=models.SET_NULL,
         null=True,
-        related_name="posts",
-        blank=True,
-        verbose_name="Местоположение",
     )
     category = models.ForeignKey(
         Category,
+        verbose_name="Категория",
         on_delete=models.SET_NULL,
         null=True,
-        related_name="posts",
-        verbose_name="Категория",
     )
+    image = models.ImageField("Изображение", blank=True, upload_to="img/")
+    objects = models.Manager()
+    post_list = PostManager()
 
     class Meta:
         verbose_name = "публикация"
         verbose_name_plural = "Публикации"
+        ordering = ("-pub_date",)
+        default_related_name = "posts"
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self) -> str:
+        return reverse("blog:post_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор",
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        verbose_name="Пост",
+    )
+    text = models.TextField(
+        verbose_name="Текст комментария",
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Дата",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = "комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ("created_at",)
+        default_related_name = "comments"
 
     def __str__(self):
-        return self.title[:SELFTITLELEN]
+        return self.text
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,3 +11,10 @@ urlpatterns = [
     path('', include('blog.urls', namespace='index')),
     path("auth/", include("django.contrib.auth.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
