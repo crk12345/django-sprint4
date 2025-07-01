6868b55a61fa2a28e84cb5994f53ceeeab7f7d2b
igor <crk@mail.ru>
2025-07-01 22:22:20 +0300

    urls

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,17 +1,21 @@
+from typing import List
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import URLPattern, include, path
 
-urlpatterns = [
+handler403 = "pages.views.permission_denied"
+handler404 = "pages.views.page_not_found"
+handler500 = "pages.views.server_error"
 
-    path('admin/', admin.site.urls),
-    path('pages/', include('pages.urls', namespace='pages')),
+urlpatterns: List[URLPattern] = [
+    path("pages/", include("pages.urls", namespace="pages")),
     path("", include("users.urls", namespace="users")),
-    path('', include('blog.urls', namespace='index')),
+    path("", include("blog.urls", namespace="blog")),
     path("auth/", include("django.contrib.auth.urls")),
+    path("admin/", admin.site.urls),
 ]
-
 if settings.DEBUG:
     import debug_toolbar
 
