cc444ce3feed34f23c66ecfc240238357ca6f7a2
igor <crk@mail.ru>
2025-07-01 20:52:06 +0300

    users2

--- blogicum/users/models.py
+++ blogicum/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    def get_absolute_url(self):
+        return reverse("blog:profile", kwargs={"username": self.username})
