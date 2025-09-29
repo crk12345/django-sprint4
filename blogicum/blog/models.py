15fe087df1549ed278a66223ef74506d66835b15
igor <crk@mail.ru>
2025-09-29 21:40:06 +0300

    auto_now=false2

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -79,8 +79,6 @@ class Post(BaseModel):
     title = models.CharField(max_length=256, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Текст")
     pub_date = models.DateTimeField(
-        #auto_now=False,
-        #auto_now_add=False,
         verbose_name="Дата и время публикации",
         help_text=(
             "Если установить дату и время в будущем — можно делать "
